fcbd06026b6fd45b036dbe2c3fb97e3a66c2a63b
liupeng <994270241@qq.com>
2026-06-30 16:59:48 +0800

    修改配置

--- src/api/gantt.js
+++ src/api/gantt.js
@@ -2,7 +2,8 @@ import { request } from '../utils/request'
 
 
 export async function getListData() {
-  const res = await request('/mock.json')
+  const base = import.meta.env.BASE_URL || '/'
+  const res = await request(`${base}mock.json`)
 
   if (res.code !== 0) {
     throw new Error(res.message || '获取数据失败')
